30e93dabd3688ef97e0e153aa684aa9f58a05eb8
anacprieto <aprieto@e-city.co>
2025-06-07 00:27:37 -0400

    GenericRepository, Create metod generic

---
+++ Projecta.IOC/Dependencia.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Projecta.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projecta.IOC
+{
+    public static class Dependencia
+    {
+
+        public static void Injection(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<BdProjectaGroupContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("cadenaSql"));
+
+            });
+
+        }
+    }
+}
